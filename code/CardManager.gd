3aeb7deb871954aefe753605cc905105abd80dc1
Samugcor <samueligcorp@gmail.com>
2026-01-05 20:56:40 +0100

    fix: card out off viewport

--- code/CardManager.gd
+++ code/CardManager.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+const COLLISION_MASK_CARD = 1
+
+var screenSize =  null
 var cardBeingDragged = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screenSize = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,15 +16,24 @@ func _process(delta: float) -> void:
 		cardBeingDragged = null
 		
 	if cardBeingDragged:
-		cardBeingDragged.position = get_global_mouse_position()
+		updateCarcCoordinates()
+		
 
 func raycastCheckCard():
 		var spaceState = get_world_2d().direct_space_state
 		var parameters = PhysicsPointQueryParameters2D.new()
 		parameters.position = get_global_mouse_position()
 		parameters.collide_with_areas = true
-		parameters.collision_mask = 1
+		parameters.collision_mask = COLLISION_MASK_CARD
 		var result = spaceState.intersect_point(parameters)
 		if result.size()>0:
 			return result[0].collider.get_parent()
 		return null
+
+func updateCarcCoordinates():
+	var mouse = get_global_mouse_position()
+	var sprite = cardBeingDragged.get_node("cardImage")
+	var offSet_y = (sprite.get_texture().get_height() * sprite.get_scale().y) /2
+	var offSet_x = (sprite.get_texture().get_width() * sprite.get_scale().x) /2
+	cardBeingDragged.position = Vector2(clamp(mouse.x, 0 + offSet_x, screenSize.x - offSet_x), 
+	clamp(mouse.y,0 + offSet_y , screenSize.y - offSet_y))
